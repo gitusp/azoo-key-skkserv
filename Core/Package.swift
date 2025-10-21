bf0c1842463135e2cd1fdad0963c4621ed504853
usp <ususso@gmail.com>
2025-10-21 22:39:18 +0900

    build: Xcode 26でビルドできるように修正
    https://github.com/azooKey/azooKey-Desktop/pull/205/files
    と同様の対応

--- Core/Package.swift
+++ Core/Package.swift
@@ -3,6 +3,26 @@
 
 import PackageDescription
 
+#if canImport(FoundationModels)
+let isXcodeVersion26 = true
+#else
+let isXcodeVersion26 = false
+#endif
+
+let xcode26AdditionalTargets: [Target] = [
+    .binaryTarget(
+        // Note: Xcode 26以降、AzooKeyKanaKanjiConverterのXCFrameworkのbinaryTargetが解決されない問題への対応
+        // 詳細は https://github.com/azooKey/azooKey-Desktop/pull/205 を参照
+        name: "llama",
+        url: "https://github.com/azooKey/llama.cpp/releases/download/b4846/signed-llama.xcframework.zip",
+        checksum: "db3b13169df8870375f212e6ac21194225f1c85f7911d595ab64c8c790068e0a"
+    )
+]
+
+let xcode26AdditionalTargetDependency: [Target.Dependency] = [
+    "llama"
+]
+
 let package = Package(
     name: "Core",
     platforms: [
@@ -28,7 +48,7 @@ let package = Package(
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
                 .product(name: "Logging", package: "swift-log"),
-            ],
+            ] + (isXcodeVersion26 ? xcode26AdditionalTargetDependency : []),
             resources: [
                 .copy("zenz-v1.gguf")
             ],
@@ -36,5 +56,5 @@ let package = Package(
                 .interoperabilityMode(.Cxx)
             ],
         ),
-    ]
+    ] + (isXcodeVersion26 ? xcode26AdditionalTargets : [])
 )
